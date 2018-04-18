f69c390fdcbb11899a56c1934113a9e4a184c09d
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-04-17 20:40:27 -0400

    added totalDifference function()

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,26 +2,39 @@
 
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-var waitListData = require("../data/friends");
+var appData = require("../data/friends");
 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(appData); //all posible friends variable willzz be places here
     });
 
-    app.post("api/friends", function (req, res) {
-        appData.push(req.body);
-
-        var comparisonArray = [];
-        //returns most compatable friend
-        for (var i = 0; i < appData.length; i++) {
-            //pushes sum of scores to comparisonArray
-            var sumOfScores = 0;
-            for (var j = 0; j < appData[i].scores.length; j++) {
-                sumOfScores += appData[i].scores[j];
+    function totalDifference() {
+        var closestMatch = appData[0];
+        var lastPersonSum = arraySum(appData[appData.length - 1]["scores"]);
+        //loops through first added to next to last added person
+        for (var i = 0; i < appData.length - 1; i++) {
+            var closestMatchSum = arraySum(closestMatch["scores"]);
+            var currentPersonSum = arraySum(appData[i]["scores"]);
+            if (Math.abs(lastPersonSum - currentPersonSum) < Math.abs(lastPersonSum - closestMatchSum)) {
+                closestMatch = appData[i];
             }
-            comparisonArray.push(sumOfScores);
         }
-        
+        return closestMatch;
+    }
+    
+    function arraySum(arr){
+        var sum = 0;
+        arr.forEach(function(num){
+            sum += num;
+        });
+        return sum;
+    }
+
+    app.post("/api/friends", function (req, res) {
+        appData.push(req.body);
+        //sends json of closest match to browser
+        console.log(totalDifference());
+        res.json(totalDifference());
     })
 }
